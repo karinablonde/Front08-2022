2c00d5868a140590c177a5b3f1fb4df83609f12b
Githug1971 <karinablonde@yahoo.com.ar>
2022-09-25 23:21:58 -0300

    ClasesBootstrap

---
+++ Clase14/JS/Variables.js
@@ -0,0 +1,15 @@
+/*
+Esto es un comentario
+en multi linea
+*/
+let valor1;
+let valor2;
+let resultado;
+
+//Asignar valores a las variables
+valor1 = 10;
+valor2 = 20;
+//Operación matematica
+resultado = valor1 + valor2;
+//muestra por consola abajo/pegado a Elemento F12
+console.log(resultado);
